52c9dc74ee2a8703804c9cd8144159961d88412d
Nadjib Amar <hello@ebabel.eu>
2018-03-03 11:35:21 +0100

    refactoring

---
+++ src/ChatServer.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+  private Socket socket = null;
+  private ServerSocket serverSocket = null;
+  private static int portNumber = 4444;
+
+  public ChatServer(int port) {
+
+  }
+}
