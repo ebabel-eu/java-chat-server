50f1f3921ec2b272ed24fcfc82af88b6540fc0fe
Nadjib Amar <hello@ebabel.eu>
2018-03-03 14:05:39 +0100

    chat server in progress

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,13 +1,30 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
   private Socket socket = null;
-  private ServerSocket serverSocket = null;
-  private static int portNumber = 4444;
+  private ServerSocket server = null;
+  private DataInputStream streamIn = null;
 
   public ChatServer(int port) {
+    System.out.println("Please wait, binding to port " + port);
 
+    try {
+      server = new ServerSocket(port);
+      System.out.println("Server started " + server);
+      System.out.println("Waiting for a client...");
+
+      socket = server.accept();
+      System.out.println("Client accepted " + socket);
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+
+  public static void main(String args[]) {
+    new ChatServer(4444);
   }
 }
