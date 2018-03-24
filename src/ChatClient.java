69f5af4ca33e4804c55eac8e0efb48fb90f73903
Nadjib Amar <hello@ebabel.eu>
2018-03-24 21:47:06 +0100

    step 4 completed but can be improved

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,34 +1,77 @@
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class ChatClient {
+public class ChatClient implements Runnable {
   private Socket socket;
   private Thread thread = null;
-  private DataInputStream streamIn = null;
+  private BufferedReader streamIn = null;
   private DataOutputStream streamOut = null;
   private ChatClientThread client = null;
 
   private ChatClient(String serverName, int serverPort) throws IOException {
-    System.out.println("[INFO] Establishing connection. Please wait...");
+    System.out.println("Loading, please wait...");
+
     socket = new Socket(serverName, serverPort);
-
-    System.out.println("[INFO] Connected");
     start();
+  }
 
-    String line = "";
+  public void run() {
+    while (thread != null) {
+      try {
+        streamOut.writeUTF(streamIn.readLine());
+        streamOut.flush();
+      } catch (IOException ex) {
+        stop();
+      }
+    }
+  }
 
-    while (!line.equals("/quit") && !line.equals("/q")) {
-      line = streamIn.readLine();
-      streamOut.writeUTF(line);
-      streamOut.flush();
+  public void handle(String message) {
+    if (message.equals("/quit") || message.equals("/q")) {
+      stop();
+    } else {
+      System.out.println(message);
     }
   }
 
   private void start() throws IOException {
-    streamIn = new DataInputStream(System.in);
+    streamIn = new BufferedReader(new InputStreamReader(System.in));
     streamOut = new DataOutputStream(socket.getOutputStream());
+
+    if (thread == null) {
+      client = new ChatClientThread(this, socket);
+      thread = new Thread(this);
+      thread.start();
+    }
+  }
+
+  public void stop() {
+    if (thread != null) {
+      thread.stop();
+      thread = null;
+    }
+
+    try {
+      if (streamIn != null) {
+        streamIn.close();
+      }
+
+      if (streamOut != null) {
+        streamOut.close();
+      }
+
+      if (socket != null) {
+        socket.close();
+      }
+    } catch (IOException ex) {
+      System.out.println("[ERROR] " + ex.getMessage());
+    }
+
+    client.close();
+    client.stop();
   }
 
   public static void main(String args[]) throws RuntimeException, IOException {
--- src/ChatClientThread.java
+++ src/ChatClientThread.java
@@ -1,3 +1,46 @@
-public class ChatClientThread extends Thread {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class ChatClientThread extends Thread {
+  private Socket socket;
+  private ChatClient client;
+  private DataInputStream streamIn = null;
+
+  public ChatClientThread(ChatClient _client, Socket _socket) {
+    client = _client;
+    socket = _socket;
+    open();
+    start();
+  }
+
+  public void open() {
+    try {
+      streamIn = new DataInputStream(socket.getInputStream());
+    } catch (IOException ex) {
+      System.out.println("[ERROR] " + ex.getMessage());
+      client.stop();
+    }
+  }
+
+  public void close() {
+    try {
+      if (streamIn != null) {
+        streamIn.close();
+      }
+    } catch (IOException ex) {
+      System.out.println("[ERROR] " + ex.getMessage());
+    }
+  }
+
+  public void run() {
+    while (true) {
+      try {
+        client.handle(streamIn.readUTF());
+      } catch (IOException ex) {
+        System.out.println("[ERROR] " + ex.getMessage());
+        client.stop();
+      }
+    }
+  }
 }
